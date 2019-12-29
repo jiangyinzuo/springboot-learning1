c5d1a4b7d03a607ec073d40fd5a2e4c12103798f
ChiangYintso <392711804@qq.com>
2019-12-29 20:18:27 +0800

    deploy

--- src/main/java/pers/jiangyinzuo/learn/controller/HelloController.java
+++ src/main/java/pers/jiangyinzuo/learn/controller/HelloController.java
@@ -1,16 +1,16 @@
 package pers.jiangyinzuo.learn.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Jiang Yinzuo
  */
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping(value = "/")
     public String home() {
-        return "Hello World!";
+        return "index";
     }
 }
